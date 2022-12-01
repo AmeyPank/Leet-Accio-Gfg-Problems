fbd10f2d3370c179f22317455a0aa3352abf871e
AmeyPank <67125423+amey438@users.noreply.github.com>
2022-12-01 14:42:07 +0530

    txt to java

--- {..ACCIO..}Set-Matrix-Zeros.java
+++ {..ACCIO..}Set-Matrix-Zeros.java
@@ -86,6 +86,7 @@ class Solution {
             //handle the first row
             for (int i = 0; i < n; i++) 
                 matrix[0][i] = 0; 
+                //git management
         } 
     } 
 }
