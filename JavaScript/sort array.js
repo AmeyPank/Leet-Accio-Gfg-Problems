040cd2bd3ae3872dad732cffaf37f5a576c624ba
AmeyPank <pankantiameya@gmail.com>
2023-01-26 18:11:44 +0530

    Sort the Array JS

---
+++ JavaScript/sort array.js
@@ -0,0 +1,35 @@
+function sortString(strArray) {
+    // return the sorted array
+     // return the sorted array
+   strArray.sort();
+      return strArray;
+  
+  
+  }
+  /*Do not change the code below */
+  
+  var readline = require("readline").createInterface(process.stdin);
+  
+  let inputArr = [];
+  var lineNumber = -1;
+  var inputSize;
+  
+  //multipleline input from User
+  readline.on("line", readInputs);
+  
+  function readInputs(line) {
+    inputArr.push(line);
+    lineNumber++;
+  
+    //Exit Condition
+    if (lineNumber == 0) {
+      logic("s");
+      readline.close();
+    }
+  }
+  
+  function logic(input) {
+    let str=JSON.parse(inputArr[0].trim());
+    console.log(sortString(str).join(" ").trim())
+  }
+  
