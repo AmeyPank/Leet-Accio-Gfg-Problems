d610b39e9e2d53e5b88cd5c91a28db5f7b388397
AmeyPank <pankantiameya@.com>
2023-04-07 11:49:02 +0530

    aVeryBigSum

---
+++ HackerRank/aVeryBigSum.js
@@ -0,0 +1,13 @@
+function aVeryBigSum(ar) {
+    // Write your code here
+  // Initialize a variable to hold the sum
+  let sum = 0;
+
+  // Loop through the array and add up the elements
+  for (let i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+
+  // Return the sum
+  return sum;
+}
