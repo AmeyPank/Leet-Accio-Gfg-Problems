90bb45af5d5d228e3ae14eb996ef057af4684fa5
AmeyPank <pankantiameya@.com>
2023-04-07 16:22:17 +0530

    miniMaxSum

--- HackerRank/miniMaxSum.js
+++ HackerRank/miniMaxSum.js
@@ -2,6 +2,10 @@
  * Complete the 'miniMaxSum' function below.
  *
  * The function accepts INTEGER_ARRAY arr as parameter.
+ * Given five positive integers, find the minimum and maximum 
+ * values that can be calculated by summing exactly four of the
+ *  five integers. Then print the respective minimum and maximum 
+ * values as a single line of two space-separated long integers.
  */
 
 function miniMaxSum(arr) {
