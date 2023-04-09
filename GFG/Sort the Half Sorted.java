ca7556212cb1cd48563081d777b03565d3f5cfb3
AmeyPank <pankantiameya@.com>
2023-04-09 20:08:46 +0530

    sort half sorted

---
+++ GFG/Sort the Half Sorted.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            
+            for (int i = 0; i < n; ++i)
+                arr[i] = sc.nextInt ();
+
+    		new Solution().sortHalves (arr, n);
+    	    for (int i = 0; i < n; ++i)
+    	        System.out.print (arr[i] + " ");
+    	    System.out.println ();
+        }
+        
+    }
+}
+
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public static void sortHalves(int arr[], int n) {
+        mergeSort(arr, 0, n - 1);
+    }
+
+    private static void mergeSort(int arr[], int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int arr[], int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+}
