afa0aa6247d00d14d7e34cd0470440c3b14b7b42
AmeyPank <pankantiameya@.com>
2023-04-18 14:38:09 +0530

    mergeAlternately

---
+++ Leetcode/Javascript/1768. Merge Strings Alternately.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function mergeAlternately(word1, word2) {
+    let merged = "";
+    const length = Math.max(word1.length, word2.length);
+    
+    for (let i = 0; i < length; i++) {
+      if (i < word1.length) {
+        merged += word1[i];
+      }
+      
+      if (i < word2.length) {
+        merged += word2[i];
+      }
+    }
+    
+    return merged;
+  }
+  ;
