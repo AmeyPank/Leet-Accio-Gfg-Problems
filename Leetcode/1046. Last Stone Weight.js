6f3aeadaed03c4b35bc570468fcf10c989f65fd9
AmeyPank <pankantiameya@.com>
2023-04-24 17:18:22 +0530

    lastStoneWeight

---
+++ Leetcode/1046. Last Stone Weight.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    while (stones.length > 1) {
+      stones.sort((a, b) => b - a);
+      const stone1 = stones.shift();
+      const stone2 = stones.shift();
+      const remaining = stone1 - stone2;
+      if (remaining > 0) {
+        stones.push(remaining);
+      }
+    }
+    return stones.length === 1 ? stones[0] : 0;
+  };
