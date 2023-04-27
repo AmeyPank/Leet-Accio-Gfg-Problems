a2eb365384f4b53ffd11bc535438f5aa0fe2cf09
AmeyPank <pankantiameya@gmail.com>
2023-04-27 10:22:02 +0530

    Super in java

---
+++ ACCIO/Java Super Keyword 1.java
@@ -0,0 +1,50 @@
+class Person
+{
+    private String name;
+    private int age;
+
+    public Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return this.name; }
+
+    public int getAge() { return this.age; }
+
+    public String toString()
+    {
+        return getName() + " " + getAge();
+    }
+
+    
+}
+
+class Teacher extends Person
+{
+    private String qualification;
+
+    public Teacher(String name, int age, String qualification)
+    {
+        super(name, age);
+        this.qualification = qualification;
+    }
+
+    public String getQualification() { return this.qualification; }
+
+    public String toString()
+    {
+        return super.toString() + " " + getQualification();
+    }
+}
+
+public class Main{
+    public static void main(String[] args)
+    {
+       Person p = new Person("Destini", 20);
+       System.out.println(p);
+       Teacher p2 = new Teacher("Erica", 55, "Masters in Teaching");
+       System.out.println(p2);
+    }
+}
