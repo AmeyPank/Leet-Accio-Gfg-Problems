18432201b07d85793747cb93b341e11d841718d5
AmeyPank <pankantiameya@gmail.com>
2023-05-21 02:40:26 +0530

    Simple A

---
+++ JavaScript/simple-arrangement.js
@@ -0,0 +1,48 @@
+function simpleArrangement(arr, n) {
+    for (let i = 0; i < n; i++) {
+      arr[i] += (arr[arr[i]] % n) * n;
+    }
+  
+    for (let i = 0; i < n; i++) {
+      arr[i] = Math.floor(arr[i] / n);
+    }
+  
+    return arr;
+  }
+  
+  
+  
+  var readline = require("readline").createInterface(process.stdin);
+  var dp = new Array(200000);
+  
+  let inputArr = [];
+  var lineNumber = -1;
+  var inputSize;
+  var K;
+  
+  //multipleline input from User
+  readline.on("line", readInputs);
+  
+  function readInputs(line) {
+    inputArr.push(line);
+    lineNumber++;
+    if (lineNumber == 0) {
+      size = parseInt(inputArr[0])
+    } else if (lineNumber < size) {
+    }
+  
+    //Exit Condition
+    if (lineNumber == 1) {
+      logic("s");
+      readline.close();
+    }
+  }
+  
+  function logic(input) {
+    const N = size;
+    inputArr.shift();
+    inputArr = inputArr[0].split(" ").map((x) => +x);
+    console.log(simpleArrangement(inputArr,N).join(" ").trim());
+  }
+  
+  
