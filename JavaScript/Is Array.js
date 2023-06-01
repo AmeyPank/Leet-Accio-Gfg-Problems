23436d2c3cf7376262f7cf4cb3a259775b65c77f
AmeyPank <pankantiameya@gmail.com>
2023-06-02 02:36:41 +0530

    function isArray(input)

---
+++ JavaScript/Is Array.js
@@ -0,0 +1,7 @@
+
+function isArray(input) {
+    // write code here
+    // use console.log to print the output
+    // console.log(typeof input)	
+    console.log(Array.isArray(input) ? "TRUE" : "FALSE");
+}
