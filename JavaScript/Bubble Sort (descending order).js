27c26fd64e1e80133b920dfe230d69b2528c8481
AmeyPank <pankantiameya@gmail.com>
2023-06-26 01:06:41 +0530

    Bubble Sort (descending order)

--- JavaScript/Bubble Sort (descending order).js
+++ JavaScript/Bubble Sort (descending order).js
@@ -1,3 +1,25 @@
+/*Bubble Sort (descending order)
+Given an array, sort the array in reverse order by simply swapping its adjacent elements.
+
+This is JS only question.
+
+Input Format
+First line of the input contains an integer, N, which denotes the length of the array. Next N inputs are elements of the array that is to be sorted in descending order.
+
+Output Format
+Output sorted array in descending order where each element is space separated.
+
+Example 1
+Input
+
+ 6
+ 3 1 2 7 9 87
+
+
+Output
+
+  87 9 7 3 2 1*/ 
+
 const BubbleSort = (arr, n) => {
   // Loop through the array
   for (let i = 0; i < n - 1; i++) {
