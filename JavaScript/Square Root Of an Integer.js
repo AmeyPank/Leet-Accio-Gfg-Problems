ac9fc829681861f226a489a01d84938a269e3be4
AmeyPank <pankantiameya@gmail.com>
2023-07-06 01:13:16 +0530

    return Math.floor(Math.sqrt(x));

--- JavaScript/Square Root Of an Integer.js
+++ JavaScript/Square Root Of an Integer.js
@@ -1,29 +1,30 @@
-// function SqrtBinary(x) {
-//       return Math.sqrt(x);
-//   }
 function SqrtBinary(x) {
-    if (x < 2) {
-        return x;
-    }
-
-    let left = 1;
-    let right = Math.floor(x / 2);
-
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        let square = mid * mid;
-
-        if (square === x) {
-            return mid;
-        } else if (square < x) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-
-    return right;
+    return Math.floor(Math.sqrt(x));
 }
+
+// function SqrtBinary(x) {
+//   if (x < 2) {
+//     return x;
+//   }
+
+//   let left = 1;
+//   let right = Math.floor(x / 2);
+
+//   while (left <= right) {
+//     let mid = Math.floor((left + right) / 2);
+//     let square = mid * mid;
+
+//     if (square === x) {
+//       return mid;
+//     } else if (square < x) {
+//       left = mid + 1;
+//     } else {
+//       right = mid - 1;
+//     }
+//   }
+
+//   return right;
+// }
 
 var readline = require("readline").createInterface(process.stdin);
 
